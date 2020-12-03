11826f0f19007156b2695df8c0e38420086979b2
beatarynkevic <beatainn@gmail.com>
2020-12-03 11:01:22 +0200

    Math lib

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import * as Matematika from './components/matematika/matematika.js';
+
+console.log(Matematika);
+
+console.log(Matematika.sumavimas(2, 48));
+console.log(Matematika.atimtis(5, 10));
+console.log(Matematika.daugyba(5, 25));
+console.log(Matematika.dalyba(12, 4));
+console.log(Matematika.kvadratu(2));
